adbc0219b075b5cbec7e01331b7ccdb0e89be0d8
Hasith Udayanga <78094049+Uhasith@users.noreply.github.com>
2024-11-09 13:04:08 +0530

    Update MultiSelect.vue

--- src/components/ui/multi-select/MultiSelect.vue
+++ src/components/ui/multi-select/MultiSelect.vue
@@ -1,45 +1,56 @@
 <script setup>
-import { CommandEmpty, CommandGroup, CommandItem, CommandList } from '@/components/ui/command'
-import { TagsInput, TagsInputInput } from '@/components/ui/tags-input'
-import { Badge } from '@/components/ui/badge'
-import { ComboboxAnchor, ComboboxContent, ComboboxInput, ComboboxPortal, ComboboxRoot } from 'radix-vue'
-import { computed, ref } from 'vue'
-import { ChevronDownIcon, X } from 'lucide-vue-next'
+import {
+    CommandEmpty,
+    CommandGroup,
+    CommandItem,
+    CommandList,
+} from '@/components/ui/command';
+import { TagsInput, TagsInputInput } from '@/components/ui/tags-input';
+import { Badge } from '@/components/ui/badge';
+import {
+    ComboboxAnchor,
+    ComboboxContent,
+    ComboboxInput,
+    ComboboxPortal,
+    ComboboxRoot,
+} from 'radix-vue';
+import { computed, ref } from 'vue';
+import { ChevronDownIcon, X } from 'lucide-vue-next';
 
 const props = defineProps({
     placeholder: {
         type: String,
-        required: false
+        required: false,
     },
     options: {
         type: Array,
         required: false,
-        default: () => []
-    }
+        default: () => [],
+    },
 });
 
-const modelValue = defineModel('modelValue')
-const open = ref(false)
-const searchTerm = ref('')
+const modelValue = defineModel('modelValue');
+const open = ref(false);
+const searchTerm = ref('');
 
 const filteredOptions = computed(() => {
-    return props.options.filter(i => !modelValue.value.includes(i.value));
+    return props.options.filter((i) => !modelValue.value.includes(i.value));
 });
 
 const findLabelById = (id) => {
-    const option = props.options.find(opt => opt.value === id);
+    const option = props.options.find((opt) => opt.value === id);
     return option ? option.label : 'Unknown';
 };
 
 // Toggle function for the popover
 const toggleOpen = () => {
-    open.value = !open.value
-}
+    open.value = !open.value;
+};
 
 const handleRemove = (value) => {
     // console.log(label)
-    modelValue.value = modelValue.value.filter(i => i !== value)
-}
+    modelValue.value = modelValue.value.filter((i) => i !== value);
+};
 </script>
 
 <template>
@@ -51,33 +62,33 @@ const handleRemove = (value) => {
                     <div class="flex items-center justify-end w-full">
                         <ComboboxInput :placeholder="props.placeholder" as-child>
                             <TagsInputInput class="w-full px-3 border-0 focus:ring-0 py-0"
-                                :class="modelValue.length > 0 ? '' : ''" @keydown.enter.prevent @focus="toggleOpen" />
+                                :class="modelValue.length > 0 ? '' : ''" @keydown.enter.prevent />
                         </ComboboxInput>
                     </div>
                 </ComboboxAnchor>
-                <ComboboxPortal>
-                    <ComboboxContent>
-                        <CommandList position="popper"
-                            class="w-[--radix-popper-anchor-width] z-[100] rounded-md mt-2 border bg-popover text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
-                            <CommandEmpty />
-                            <CommandGroup>
-                                <CommandItem v-for="option in filteredOptions" :key="option.value" :value="option.value"
-                                    @select.prevent="(ev) => {
+
+                <ComboboxContent>
+                    <CommandList position="popper"
+                        class="w-[--radix-popper-anchor-width] z-[100] rounded-md mt-2 border bg-popover text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
+                        <CommandEmpty />
+                        <CommandGroup>
+                            <CommandItem v-for="option in filteredOptions" :key="option.value" :value="option.value"
+                                @select.prevent="(ev) => {
                                         if (modelValue.includes(ev.detail.value)) {
-                                            return
+                                            return;
                                         } else {
-                                            modelValue.push(ev.detail.value)
+                                            modelValue.push(ev.detail.value);
                                         }
                                         if (filteredOptions.length === 0) {
-                                            open = false
+                                            open = false;
                                         }
-                                    }">
-                                    {{ option.label }}
-                                </CommandItem>
-                            </CommandGroup>
-                        </CommandList>
-                    </ComboboxContent>
-                </ComboboxPortal>
+                                    }
+                                    ">
+                                {{ option.label }}
+                            </CommandItem>
+                        </CommandGroup>
+                    </CommandList>
+                </ComboboxContent>
             </ComboboxRoot>
         </TagsInput>
         <div v-if="modelValue.length > 0" class="flex gap-2 flex-wrap items-center px-3 py-3">
