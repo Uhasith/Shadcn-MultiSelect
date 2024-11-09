0e2cc3327ff107051cb5fb74e8c9673bec5a886a
Uhasith <git@stackblitz.com>
2024-11-09 12:13:15 +0530

    Initial commit

---
+++ src/components/DialogModal.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref, watch } from 'vue';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { MultiSelect } from '@/components/ui/multi-select';
+
+const selectedOptions = ref([]);
+const selectOptions = [
+  { label: 'Option 1', value: '1' },
+  { label: 'Option 2', value: '2' },
+];
+
+const showModal = defineModel('showModal');
+</script>
+
+<template>
+  <Dialog v-model:open="showModal">
+    <DialogContent
+      class="sm:max-w-xl grid-rows-[auto_minmax(0,1fr)_auto] p-0 max-h-[80dvh]"
+      @interactOutside="(e) => e.preventDefault()"
+    >
+      <DialogHeader class="p-6 pb-0">
+        <DialogTitle>Add Form</DialogTitle>
+      </DialogHeader>
+      <div class="grid gap-4 py-4 overflow-y-auto px-6">
+        <div class="flex flex-col justify-between">
+          <form id="add-record" @submit="onSubmit">
+            <div class="mb-4 mt-2">
+              <div class="w-1/2">
+                <MultiSelect
+                  placeholder="Select"
+                  :options="selectOptions"
+                  v-model:modelValue="selectedOptions"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <DialogFooter class="p-6 pt-0">
+        <div class="px-4 py-3 text-right">
+          <button
+            form="add-record"
+            type="submit"
+            class="bg-green1 text-base font-medium text-white w-[139px] rounded-3xl px-6 py-2"
+          >
+            Add
+          </button>
+        </div>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+import DialogModal from '@/components/DialogModal.vue';
+import { Button } from '@/components/ui/button';
+import { MultiSelect } from '@/components/ui/multi-select';
+defineProps({
+  msg: String,
+});
+
+const show = ref(false);
+const selectedOptions = ref([]);
+const selectOptions = [
+  { label: 'Option 1', value: '1' },
+  { label: 'Option 2', value: '2' },
+];
+</script>
+
+<template>
+  <div class="flex flex-col item-center justify-center p-8">
+    <div class="w-1/2">
+      <MultiSelect
+        placeholder="Select Sensors"
+        :options="selectOptions"
+        v-model:modelValue="selectedOptions"
+      />
+    </div>
+    <div class="mt-4">
+    <Button @click="show = true">Open Dialog</Button>
+  </div>
+  </div>
+  <DialogModal v-model:showModal="show" />
+</template>
